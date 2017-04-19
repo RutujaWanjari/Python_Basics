86d334f1fab9ddb16aa65246100d09c487e39a3d
Rutuja <rutuja.wanjari2@gmail.com>
2017-04-19 14:03:10 +0530

    Add more shapes

--- draw_shapes.py
+++ draw_shapes.py
@@ -7,7 +7,7 @@ window.bgcolor("Red")
 brad = turtle.Turtle()
 brad.color("Yellow")
 brad.shape("turtle")
-brad.speed(1)
+brad.speed(3)
 
 
 def square():
@@ -23,11 +23,24 @@ def circle():
 def triangle():
     brad.left(180)
 
-    for i in range(0, 3):
+    for j in range(0, 3):
         brad.forward(100)
         brad.left(120)
+
+
+def art():
+    brad.right(20)
+    brad.color("Red")
+    brad.forward(300)
+    brad.color("Yellow")
+
+    for i in range(0, 36):
+        brad.right(10)
+        square()
 
 square()
 circle()
 triangle()
+art()
+
 window.exitonclick()

e84d5f742ff4c53d8109ce1e2549a139c9be8401
rutu <rutu@pop-os.localdomain>
2020-09-07 21:24:46 +0530

    exercise

---
+++ excercise.py
@@ -0,0 +1,21 @@
+# Excercise which I couldn't complete
+
+# 1
+a = 45
+b = 15
+c = 3
+ 
+print(a - b / c)
+
+# 2
+quantity = 10
+price = 5.0
+total = quantity * price
+tax = total / 5
+ 
+Total = total + tax
+print(total)
+
+# 3
+days = "Mon, Tue, Wed, Thu, Fri, Sat, Sun"
+print(days[::5])
